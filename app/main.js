137f7687d66e1edbe9a719f076723e754ec65869
Pierre Martinet <pierre.martinet@etu.umontpellier.fr>
2018-11-08 18:20:49 +0100

    robot texture from opengameart

--- app/main.js
+++ app/main.js
@@ -20,7 +20,7 @@ var config = {
 
 const game = new Phaser.Game(config);
 let controls;
-let player;
+let robot;
 let cursors;
 const speed = 200;
 
@@ -28,9 +28,9 @@ function preload()
 {
 	this.load.image('tiles', "asset/roguelike.png");
 	this.load.tilemapTiledJSON('map', "asset/map.json");
-	this.load.spritesheet('player',
+	this.load.spritesheet('robot',
 		"asset/robot.png",
-        {frameWidth: 16, frameHeight: 16, endFrame: 0});
+        {frameWidth: 32, frameHeight: 32, endFrame: 2});
 }
 
 function create()
@@ -45,11 +45,16 @@ function create()
 
 	objects_layer.setCollisionByProperty({ obstacle: true });
 
-	player = this.physics.add.sprite(38, 38, 'player').setScale(1.5);
-	this.physics.add.collider(player, objects_layer);
+	robot = this.physics.add.sprite(38, 38, 'robot');
+
+	this.physics.add.collider(robot, objects_layer);
+
+	this.anims.create({ key: 'walking',
+		                frames: this.anims.generateFrameNumbers('robot', {start: 0, end: 2}),
+		                frameRate: 20 });
 
 	camera = this.cameras.main;
-	camera.startFollow(player);
+	camera.startFollow(robot);
 	camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 	camera.setZoom(2.0);
 
@@ -58,17 +63,22 @@ function create()
 
 function update(time, dt)
 {
-	player.body.setVelocity(0);
+	robot.body.setVelocity(0);
 
 	if(cursors.left.isDown)
-		player.body.setVelocityX(-100);
+		robot.body.setVelocityX(-100);
 	else if(cursors.right.isDown)
-		player.body.setVelocityX( 100);
+		robot.body.setVelocityX( 100);
 
 	if(cursors.up.isDown)
-		player.body.setVelocityY(-100);
+		robot.body.setVelocityY(-100);
 	else if(cursors.down.isDown)
-		player.body.setVelocityY( 100);
+		robot.body.setVelocityY( 100);
 
-	player.body.velocity.normalize().scale(speed);
+	robot.body.velocity.normalize().scale(speed);
+
+	if(robot.body.velocity.length() == 0.0)
+		robot.anims.stop();
+	else
+		robot.anims.play('walking', true);
 }
