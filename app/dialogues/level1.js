83a27502f51c187087265cebc78dfdaa3bba5949
Pierre Martinet <pierre.martinet@etu.umontpellier.fr>
2018-12-05 17:23:34 +0100

    robocom library

--- app/dialogues/level1.js
+++ app/dialogues/level1.js
@@ -24,6 +24,29 @@ const DLG_LEVEL1 = [
 	},
 
 	{
-		text: ["blah"],
+		text: [
+			"But that's not important. What's important is, you're going to pilot it.",
+			"I'm going to send you a library of functions to pilot the robot.",
+			"But first, you'll have to install another library that takes care of communications.",
+			"Type the following command: <span class='code'>stack build websockets</span>",
+			"Now download my library: <a href='assets/robocom.hs'>robocom.hs</a>",
+		],
+
+		answers: {
+			"Done!": [
+				{text: ["Perfect!"]},
+			],
+
+			"Wait, you can send me files? Why don't you send the solution directly?": [
+				{ text: [
+					"I could, but where would be the fun for you?",
+					"Consider it my way of saying thanks: you help me and in exchange I teach you Haskell.",
+				] },
+			]
+		}
 	},
+
+// 	{
+// 		action: [["enable_note", "The Robot library"]],
+// 	}
 ];
